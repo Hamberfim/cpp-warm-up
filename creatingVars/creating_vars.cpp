534906ce35cdd9c523bc8b1e5b4e103a4b29c7db
Hamberfim <adhamlingithub@gmail.com>
2024-01-04 16:01:59 -0600

    warm up

---
+++ creatingVars/creating_vars.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // declare and initialize
+    char letter = 'A';
+    int iNum = 42;
+    float fNum = 7.5;
+    double dNum = 3.14159;
+    bool isTrue = false;
+
+    cout << "Char: " << letter << endl;
+    cout << "Integer: " << iNum << endl;
+    cout << "Float: " << fNum << endl;
+    cout << "Double: " << dNum << endl;
+    cout << "Bool: " << isTrue << endl;
+
+    return 0;
+}
