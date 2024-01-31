76d398e7d5659c1b520eaf78c52fbbf87d8ff5cc
Hamberfim <adhamlingithub@gmail.com>
2024-01-31 15:32:46 -0600

    ternary

--- logicBranching/ternaryCondition.cpp
+++ logicBranching/ternaryCondition.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
+#include <vector>
 
-int main(void) { return 0; }
+using namespace std;
+
+int main(void){
+    vector <int> numArr{10, 13, 15, 16, 17, 22, 56, 3};
+    int varr_length = (int)numArr.size();
+
+    for (int i = 0; i < varr_length; i++)
+    {
+        cout << numArr[i] << " % 2 is " << ((numArr[i]%2 != 0) ? "odd" : "even") << endl;
+    }
+    
+    return 0;
+}
