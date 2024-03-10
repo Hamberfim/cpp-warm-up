b3d7e2f189e20bfc3656a7c46c8e0302380fea68
Hamberfim <adhamlin@gmail.com>
2024-03-10 11:38:25 -0500

    pract

---
+++ __practise__/CircleCalc.cpp
@@ -0,0 +1,17 @@
+/* Author: Hamberfim -via modification
+ * This program calculates a circumference
+ */
+#include <cmath>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::pow;
+using std::round;
+
+int main(void) {
+
+  // display what the program is
+  cout << "=== Circle Calculator ===" << endl << endl;
+}
