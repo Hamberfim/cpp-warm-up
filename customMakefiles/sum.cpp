6afc22c3380400bda11f51f38dd9d3cd7b31e2b2
hamberfim <adhamlin@gmail.com>
2024-03-11 14:52:34 -0500

    testing makefile

---
+++ customMakefiles/sum.cpp
@@ -0,0 +1,17 @@
+/* testing custom make file */
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+  int num1 = 15;
+  int num2 = 12;
+  char arr[3] = {'z', 'x', 'y'};
+
+  cout << "Total sum is  " << num1 + num2 << endl;
+  cout << arr[2] << " ";
+  cout << arr[1] << " ";
+  cout << arr[0] << endl;
+  return 0;
+}
